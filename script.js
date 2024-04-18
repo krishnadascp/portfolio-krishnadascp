560bddc1cef2eb68f4701fb9ad2bc09b89047109
krishnadascp <krishnadascp1999@gmail.com>
2024-04-18 21:01:44 +0530

    rhj

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ function openTab(tabname) {
     event.currentTarget.classList.add('active-link');
     document.getElementById(tabname).classList.add('active-tab');
 }
+
+var sidemenu = document.getElementById('sidemenu');
+function openMenu() {
+    sidemenu.style.right = '0';
+}
+function closeMenu() {
+    sidemenu.style.right = '-200px';
+}
