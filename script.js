03455c91d8522cced5efaf9bf6b319367a219d2d
krishnadascp <krishnadascp1999@gmail.com>
2024-04-19 16:02:33 +0530

    rhshrt

--- script.js
+++ script.js
@@ -18,20 +18,3 @@ function openMenu() {
 function closeMenu() {
     sidemenu.style.right = '-200px';
 }
-
-function sendEmail() {
-    Email.send({
-        // secureToken: "671b70d1-e2d8-47e5-92ce-a728bcbd912f",
-        Host : "smtp.gmail.com",
-        Username : "krishnadascp1999@gmail.com",
-        Password : "BF10F39F8F1A05BEFB0F634C43D2301BBB03",
-        To : 'krishnadascp1999@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "Portfolio - Enquiry",
-        Body: "Name: " + document.getElementById("name").value
-            + "<br> Email: " + document.getElementById("email").value
-        + "<br> Message: " + document.getElementById("message").value
-    }).then(
-        message => alert("email sent successfully")
-    );
-}
